1822159bc5424c9210c15c89afd3b7b9eac1ff31
LordGrim96 <strrnea09@bbzzuegg.it>
2026-04-15 14:07:11 +0200

    Add database configuration in config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+$host = getenv("DB_HOST");
+$db   = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+
+try {
+  $pdo = new PDO(
+    "mysql:host=$host;dbname=$db;charset=utf8mb4",
+    $user,
+    $pass,
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+  );
+} catch (PDOException $e) {
+  die("DB Fehler");
+}

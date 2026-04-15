ab5a82efc20af0f13c2d0d1e3f1754d30ed8dad4
LordGrim96 <strrnea09@bbzzuegg.it>
2026-04-15 14:14:02 +0200

    Update save_day.php

--- save_day.php
+++ save_day.php
@@ -1,15 +1,23 @@
 <?php
-require "config.php)"require "config.php";
-);
+require "config.php";
+
+// JSON einlesen
+$data = json_decode(file_get_contents("php://input"), true);
+
+if (!isset($data["user"], $data["date"])) {
+  http_response_code(400);
+  exit;
+}
+
+// Insert oder ignorieren (kein doppelter Tag)
+$stmt = $pdo->prepare("
+  INSERT IGNORE INTO gym_days (user_name, gym_date)
+  VALUES (?, ?)
+");
+
 $stmt->execute([
   $data["user"],
   $data["date"]
 ]);
 
 echo json_encode(["status" => "ok"]);
-
-
-$data = json_decode(file_get_contents("php://input"), true);
-
-$stmt = $pdo->prepare(
-  "INSERT INTO gym_days (user_name, gym_date)
